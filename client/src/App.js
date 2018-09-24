83cc0eb28a90f1148c169e6ef7c6534b9dae9a72
hexie <hx3329@gmail.com>
2018-09-24 15:00:50 +1000

    format app and add withRouter

--- client/src/App.js
+++ client/src/App.js
@@ -35,41 +35,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 
 
 
-// const FadingRoute = ({ component: Component, ...rest }) => (
-//   <Route {...rest} render={props => (
-//     <FadeIn>
-//       <Component {...props}/>
-//     </FadeIn>
-//   )}/>
-// )
-
 class App extends React.Component{
-    // constructor(props) {
-    //   super(props);
-    //   this.state = {
-    //     authenticated: false
-    //   }
-    //
-    // this.authenticate=this.authenticate.bind(this);
-    // this.logout=this.logout.bind(this);
-
-    // }
-
-    // componentDidMount(){
-    //   const object = getFromStorage('the_main_app');
-    //   if(object && object.token) {
-    //     this.setState({authenticated:true});
-    //   }
-    // }
-
-    // authenticate(){
-    //   this.setState( {authenticated: true} );
-    // };
-    //
-    // logout(){
-    //   this.setState({authenticated: false});
-    // };
-
     render(){
         return(
             <Router>
@@ -77,17 +43,12 @@ class App extends React.Component{
                     <Switch>
                         <Route exact path="/" component={Home}/>
                         <Route path="/about" component={About}/>
-                        {/*<Route path="/group" component={Group}/>*/}
                         <Route path="/login" component={Signin} />
                         <Route path="/signup" component={Register}/>
                         <Route path="/work" component={Work}/>
                         <Route path="/map" component={MapPage}/>
                         <PrivateRoute path="/group" component={Group}/>
                         <Route component={NotFound}/>
-                        {/*<FadingRoute path="/group" component={Group}/>*/}
-                        {/*<AuthRoute path="/group" component={Group} redirectTo="/login" authenticated={this.state.authenticated} />*/}
-                        {/*<Route path="/group" render={() => <Group />}/>*/}
-
                     </Switch>
                 </Layout>
             </Router>
