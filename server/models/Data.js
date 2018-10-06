c79603a80efb0c7abd0928ade3963cf93ed6a04c
hexie <hx3329@gmail.com>
2018-10-06 13:46:29 +1000

    add new data model for coordinates

---
+++ server/models/Data.js
@@ -0,0 +1,91 @@
+const mongoose = require('mongoose');
+// const unique = require('mongoose-unique-validator');
+const validate = require('mongoose-validator');
+
+const AirSpaceClassValidator = [
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'AirSpaceClass must not exceed {ARGS[1]} characters.'
+    })
+];
+
+const From_CityValidator = [
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'From_City must not exceed {ARGS[1]} characters.'
+    }),
+];
+
+const To_CityValidator = [
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'To_City must not exceed {ARGS[1]} characters.'
+    })
+];
+
+const PriceValidator = [
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'Price must not exceed {ARGS[1]} characters.'
+    })
+];
+
+const AircraftModelValidator =[
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'AircraftModel must not exceed {ARGS[1]} characters.'
+    })
+];
+
+const EngineModelValidator =[
+    validate({
+        validator: 'isLength',
+        arguments: [0, 40],
+        message: 'EngineModel must not exceed {ARGS[1]} characters.'
+    })
+];
+// Define the database model
+const DataSchema = new mongoose.Schema({
+    AirSpaceClass: {
+        type: String,
+        required: [true, 'AirSpaceClass is required.'],
+        validate: AirSpaceClassValidator
+    },
+    From_City: {
+        type: String,
+        required: [true, 'From_City is required.'],
+        // unique: true,
+        validate: From_CityValidator
+    },
+    To_City: {
+        type: String,
+        required: [true, 'To_City is required.'],
+        validate: To_CityValidator
+    },
+    Price: {
+        type: String,
+        required: [true, 'Price is required.'],
+        validate: PriceValidator
+    },
+    AircraftModel:{
+        type:String,
+        required: [true, 'AircraftModel is required.'],
+        validate: AircraftModelValidator
+    },
+    EngineModel:{
+        type:String,
+        required: [true, 'EngineModel is required.'],
+        validate: EngineModelValidator
+    }
+
+});
+
+// Use the unique validator plugin
+// DataSchema.plugin(unique, { message: 'That {PATH} is already taken.' });
+
+module.exports = mongoose.model('Data', DataSchema);
